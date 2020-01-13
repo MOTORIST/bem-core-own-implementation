b6a0b5d85806d30328d34f67c0ca6e9c9c212243
Gerasin Ivan <webartmotor@gmail.com>
2020-01-14 01:16:05 +0300

    create bemMod func

--- src/own-bem-core/index.ts
+++ src/own-bem-core/index.ts
@@ -8,9 +8,30 @@ export type Enhance<T extends IClassNameProps> = (
   WrappedComponent: ComponentType<T>,
 ) => ComponentType<T>;
 
+interface IModValue<V, EP extends IClassNameProps = {}> {
+  value: V;
+  enhance?: Enhance<EP>;
+}
+
+interface IMod<N extends string, VS extends IModValue<string>[]> {
+  name: N;
+  values: VS;
+}
+
+export type GetPropsOfMod<
+  M extends IMod<string, IModValue<string>[]>
+> = M['values'][number]['value'];
+
 export function bemModValue<V extends string, EP extends IClassNameProps>(
   value: V,
   enhance?: Enhance<EP>,
-) {
+): IModValue<V, EP> {
   return { value, enhance };
+}
+
+export function bemMod<N extends string, V extends IModValue<string>>(
+  name: N,
+  values: V[],
+): IMod<N, V[]> {
+  return { name, values };
 }
